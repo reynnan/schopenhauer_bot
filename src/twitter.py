aa94b4c56a898a225127e11cfb606a10454b41b1
Reynnan Viktor <reynnan.viktor@sambla.se>
2020-01-26 16:17:31 +0100

    Moving globla var to local function scope

--- src/twitter.py
+++ src/twitter.py
@@ -10,7 +10,6 @@ CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_KEY_SECRET = os.getenv("CONSUMER_KEY_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
-MAX_CHARACTER = 258
 
 def auth_to_twitter(consumer_key: str, consumer_key_secret: str, access_token: str, access_token_secret: str):
     auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
@@ -20,14 +19,15 @@ def auth_to_twitter(consumer_key: str, consumer_key_secret: str, access_token: s
 def create_twitter_api_object(auth):
     return tweepy.API(auth)
 
-def should_create_thread(quote: str) -> int:
-    return ceil(len(quote) / MAX_CHARACTER) > 1
+def should_create_thread(quote: str, max_character: int) -> int:
+    return ceil(len(quote) / max_character) > 1
 
 def tweet_new_quote(send_tweet: Callable[[str], object], quote: str, reply_tweet_id: str = None) -> object:
+    MAX_CHARACTER = 258
     formatted_quote = quote[0:MAX_CHARACTER]
     tweeted_quote = send_tweet(formatted_quote, reply_tweet_id)
     
-    if should_create_thread(quote):
+    if should_create_thread(quote, MAX_CHARACTER):
         tweet_new_quote(send_tweet, quote[MAX_CHARACTER:len(quote)], tweeted_quote.id_str)
     
     return tweeted_quote
